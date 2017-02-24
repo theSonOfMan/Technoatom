8cb5a971feb197ad0f375eae1bffa8f7fc2305ee
theSonOfMan <petr.galaburda@gmail.com>
2017-02-24 18:23:37 +0300

    создан каркас класса стэк

---
+++ stack_test.cpp
@@ -0,0 +1,14 @@
+//------------------------------
+//!@file stack_test
+//!
+//!@author theSonOfMan
+//------------------------------
+
+
+#include <iostream>
+using namespace std;
+#include "stack_class.h"
+
+int main(int argc, char *argv[]) {
+	
+}
